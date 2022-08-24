b7e819760cc100f1d42c149d14110b7b4d3f3107
Farhan Franaka <farhanfranaka0505@gmail.com>
2022-08-24 10:52:04 +0700

    Handling Form

--- script.js
+++ script.js
@@ -10,18 +10,21 @@ const sumbitform = (e) => {
     const ipc = document.getElementById("ipc").value
     const ipr = document.getElementById("ipr")
     const notif = document.getElementById("notif")
+    const ucapan1 = document.getElementById("ucapan1")
     const alert = document.getElementById("alert")
 
     var arrayTest = [ips, sks1, sks2, ipc]
     var test = true
     for (var i = 0; i <= 3; i++){
-        if (arrayTest[i] == " "){
+        if (arrayTest[i] === ""){
             test = false
             break
         }
     }
+    console.log(test)
 
     if(test){
+        alert.style.display = "none"
 
         var nilaiTem = ips * sks1
         var sksSisa = sks2 - sks1
@@ -33,15 +36,18 @@ const sumbitform = (e) => {
         if(result < 4){
             notif.classList.remove("none")
             ipr.innerHTML = result
+            ucapan1.innerHTML = "Minimal nilai per-sks yang dibutuhkan untuk bisa cumlaude adalah"
         }
         else{
             notif.classList.remove("none")
+            ucapan1.innerHTML = "Waduhhh"
             ipr.innerHTML = "Udah gaada harapan cumlude kk"
         }
     }
 
     else{
         alert.style.display = "flex"
+
     }
 
     
